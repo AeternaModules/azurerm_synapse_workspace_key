1ef42d579933bb8224ec5024fa345c303e7ec2a0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "synapse_workspace_keys" {
-  description = "All synapse_workspace_key resources"
-  value       = azurerm_synapse_workspace_key.synapse_workspace_keys
-}
 output "synapse_workspace_keys_active" {
-  description = "List of active values across all synapse_workspace_keys"
-  value       = [for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : v.active]
+  description = "Map of active values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.active }
 }
 output "synapse_workspace_keys_customer_managed_key_name" {
-  description = "List of customer_managed_key_name values across all synapse_workspace_keys"
-  value       = [for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : v.customer_managed_key_name]
+  description = "Map of customer_managed_key_name values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_name }
 }
 output "synapse_workspace_keys_customer_managed_key_versionless_id" {
-  description = "List of customer_managed_key_versionless_id values across all synapse_workspace_keys"
-  value       = [for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : v.customer_managed_key_versionless_id]
+  description = "Map of customer_managed_key_versionless_id values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_versionless_id }
 }
 output "synapse_workspace_keys_synapse_workspace_id" {
-  description = "List of synapse_workspace_id values across all synapse_workspace_keys"
-  value       = [for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : v.synapse_workspace_id]
+  description = "Map of synapse_workspace_id values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.synapse_workspace_id }
 }
 
