77507970edbb261edc4edb719d039fc7c7b595c3
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:44 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "synapse_workspace_keys_id" {
   description = "Map of id values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
-  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.id }
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "synapse_workspace_keys_active" {
   description = "Map of active values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
-  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.active }
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.active if v.active != null }
 }
 output "synapse_workspace_keys_customer_managed_key_name" {
   description = "Map of customer_managed_key_name values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
-  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_name }
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_name if v.customer_managed_key_name != null && length(v.customer_managed_key_name) > 0 }
 }
 output "synapse_workspace_keys_customer_managed_key_versionless_id" {
   description = "Map of customer_managed_key_versionless_id values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
-  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_versionless_id }
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.customer_managed_key_versionless_id if v.customer_managed_key_versionless_id != null && length(v.customer_managed_key_versionless_id) > 0 }
 }
 output "synapse_workspace_keys_synapse_workspace_id" {
   description = "Map of synapse_workspace_id values across all synapse_workspace_keys, keyed the same as var.synapse_workspace_keys"
-  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.synapse_workspace_id }
+  value       = { for k, v in azurerm_synapse_workspace_key.synapse_workspace_keys : k => v.synapse_workspace_id if v.synapse_workspace_id != null && length(v.synapse_workspace_id) > 0 }
 }
 
